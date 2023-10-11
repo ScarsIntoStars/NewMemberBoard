cd14bc07e5b95b777024b4d905816e8a05e3d9d0
ScarsIntoStars <conpana_jj@naver.com>
2023-10-11 13:28:32 +0900

    01. 프로젝트 세팅

---
+++ src/test/java/com/icia/newmemberboard/NewMemberBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icia.newmemberboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewMemberBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
